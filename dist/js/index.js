44ede979e29caeee67298f4425215dbea544211e
NguyenRobin <robin.nnguyen@hotmail.com>
2022-12-23 14:11:11 +0100

    done

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,13 +1,18 @@
 import { getData } from './modules/api.js';
 import { clickEscape } from './modules/helper-functions.js';
-import { createEachBook, InputSearch, clickedBook, clickEnter, } from './modules/display-functions.js';
+import {
+  createEachBook,
+  InputSearch,
+  clickedBook,
+  clickEnter,
+} from './modules/display-functions.js';
 async function loadPage() {
-    const dataOfBooks = await getData();
-    createEachBook(dataOfBooks);
-    clickedBook(dataOfBooks);
-    InputSearch(dataOfBooks);
-    clickEnter(dataOfBooks);
-    clickEscape();
+  const dataOfBooks = await getData();
+  createEachBook(dataOfBooks);
+  clickedBook(dataOfBooks);
+  InputSearch(dataOfBooks);
+  clickEnter(dataOfBooks);
+  clickEscape();
 }
 loadPage();
 // Robin Nguyen
